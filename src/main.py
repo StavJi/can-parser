c4ec5da6e3c85d61fdfa34fd6c1fa27d2c669999
Jiří Stavělík <jiri.stavelik@veit.cz>
2025-09-18 15:37:05 +0200

    feat: add draft of frame select (does not work yeat)

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from frame_parser import FrameParser
 
 f = open("output.txt", "w")
 
-def run_parser(filename: str, output_file: str = "output.txt"):
+def run_parser(filename: str, selected_frames, output_file: str = "output.txt"):
     """ Main parsing logic """
     with open(output_file, "w") as f:
         gen = CanLogParser.generator(filename, CanLogParser.parse_line_h407_logger)
@@ -14,7 +14,7 @@ def run_parser(filename: str, output_file: str = "output.txt"):
         for frame  in gen:
             if frame.time_s is not None and frame.short_id is not None:
 
-                if frame.short_id == 0x00FF07 and frame.channel == '1': # ACUSTATUS channel 1
+                if frame.short_id == 0x00FF07 and frame.channel == '1' in selected_frames: # ACUSTATUS channel 1
                    frm = FrameParser.parse_acu_status(frame.channel, frame.payload)
                    f.write(f"Engine1 state: {frm['diesel_state_1_s']}")
                    f.write(';')
@@ -23,7 +23,7 @@ def run_parser(filename: str, output_file: str = "output.txt"):
                    f.write('\n')
 
 
-                if frame.short_id == 0x00FF07 and frame.channel == '2': # ACUSTATUS channel 1
+                if frame.short_id == 0x00FF07 and frame.channel == '2' in selected_frames: # ACUSTATUS channel 1
                    frm = FrameParser.parse_acu_status(frame.channel, frame.payload)
                    f.write(f"Engine2 state: {frm['diesel_state_2_s']}")
                    f.write(';')
@@ -31,40 +31,40 @@ def run_parser(filename: str, output_file: str = "output.txt"):
                    f.write(f"Colling: {frm['cooling_active_2']}")
                    f.write('\n')
 
-                if frame.short_id == 0x00FF0C and frame.channel == '1': # EMCUERROR channel 1
+                if frame.short_id == 0x00FF0C and frame.channel == '1' in selected_frames: # EMCUERROR channel 1
                     frm = FrameParser.parse_emcu_error(frame.channel, frame.payload)
                     f.write(f"Chrg 1: {frm['Elmot_Charge_Error_1']}")
                     f.write('\n')
                     f.write(f"Err 1: {frm['Elmot_Error_1']}")
                     f.write('\n')
 
-                if frame.short_id == 0x00FF0C and frame.channel == '2': # EMCUERROR channel 2
+                if frame.short_id == 0x00FF0C and frame.channel == '2' in selected_frames: # EMCUERROR channel 2
                     frm = FrameParser.parse_emcu_error(frame.channel, frame.payload)
                     f.write(f"Chrg 2: {frm['Elmot_Charge_Error_2']}")
                     f.write('\n')
                     f.write(f"Err 2: {frm['Elmot_Error_2']}")
                     f.write('\n')
 
-                if frame.short_id == 0x00FF06 and frame.channel == '1': # channel 1
+                if frame.short_id == 0x00FF06 and frame.channel == '1' in selected_frames: # channel 1
                     frm = FrameParser.parse_tmc2emcu(frame.channel, frame.payload)
                     f.write(f"CH1 running {frm['elmot_running_1']}")
                     f.write('\n')
                     f.write(f"CH1 charging {frm['elmot_charging_running_1']}")
                     f.write('\n')
 
-                if frame.short_id == 0x00FF06 and frame.channel == '2': # channel 2
+                if frame.short_id == 0x00FF06 and frame.channel == '2' in selected_frames: # channel 2
                     frm = FrameParser.parse_tmc2emcu(frame.channel, frame.payload)
                     f.write(f"CH2 running {frm['elmot_running_2']}")
                     f.write('\n')
                     f.write(f"CH2 charging {frm['elmot_charging_running_2']}")
                     f.write('\n')
 
-                if frame.short_id == 0x00FF08 and frame.channel == '1': # channel 1
+                if frame.short_id == 0x00FF08 and frame.channel == '1' in selected_frames: # channel 1
                     frm = FrameParser.parse_emcu_status(frame.channel, frame.payload)
                     f.write(f"CH1 status {frm['EMCU_State_1']}")
                     f.write('\n')
 
-                if frame.short_id == 0x00FF08 and frame.channel == '2': # channel 1
+                if frame.short_id == 0x00FF08 and frame.channel == '2' in selected_frames: # channel 1
                     frm = FrameParser.parse_emcu_status(frame.channel, frame.payload)
                     f.write(f"CH2 status {frm['EMCU_State_2']}")
                     f.write('\n')
@@ -86,7 +86,8 @@ if __name__ == "__main__":
 
         try:
             app.log("Analyzing, please wait...")
-            run_parser(app.filepath)
+            selected_frames = [name for name, var in app.check_vars.items() if var.get()]
+            run_parser(app.filepath, selected_frames)
             app.log("Parsing done! See output.txt")
         except Exception as e:
             # Parsing error
