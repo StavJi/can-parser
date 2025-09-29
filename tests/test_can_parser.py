5a69b8a5e4baf808fe8f59b5d5883fecc828ab1c
Jiří Stavělík <jiri.stavelik@veit.cz>
2025-09-29 14:34:46 +0200

    feat: add draft of more tests

--- tests/test_can_parser.py
+++ tests/test_can_parser.py
@@ -72,19 +72,201 @@ def test_parse_tmc_status():
     assert "DATE:" in result["tmc_dt"]
     assert result["dplus"] == 1
 
+def test_parse_tmc_status2():
+    # return {'RTT_timestamp': payload[2] | (payload[3] << 8),  # 3-4 N/A Timestamp (sec)
+    #         'TMC_ResetCounter': payload[4] | (payload[5] << 8),  # 5-6 N/A TMC Reset Counter (sec)
+    #         'TMC_Watchog_Counter': payload[6] | (payload[7] << 8)}  # 7-8 N/A TMC Watchog Counter (sec)
+    pass
+
 def test_parse_tmc_status3():
     payload = [5, 80, 0, 0, 0, 0, 0, 0]  # Test data
     result = FrameParser.parse_tmc_status3(payload)
+    # data = {}
+    # for i in range(4):
+    #     data[f'PowerSource_Status_{i + 1}'] = payload[i] & 0x07
+    #     data[f'PowerSource_Status_{i + 1}_s'] = FrameParser.gen_pwr_status_name(payload[i] & 0x07)
+    #     data[f'PowerControl_State_{i + 1}'] = (payload[i] >> 3) & 0x07
+    #     data[f'PowerControl_State_{i + 1}_s'] = FrameParser.gen_pwr_state_name((payload[i] >> 3) & 0x07)
+    #     data[f'PowerControl_IsError_{i + 1}'] = (payload[i] >> 6) & 0x01
+    #
+    # return data
     assert "PowerSource_Status_1" in result
     assert isinstance(result["PowerControl_State_1_s"], str)
 
+def test_parse_tmc_status4():
+    # data = {}
+    # for i in range(4):
+    #     data[f'Charger_State_{i + 1}'] = payload[i] & 0x07
+    #     data[f'Charger_State_{i + 1}_s'] = FrameParser.gen_load_states(payload[i] & 0x07)
+    #     data[f'Charger_IsError_{i + 1}'] = (payload[i] >> 6) & 0x01
+    #     data[f'Charger_IsActivated_{i + 1}'] = (payload[i] >> 7) & 0x01
+    #
+    #     data[f'Cooler_State_{i + 1}'] = payload[i + 4] & 0x07
+    #     data[f'Cooler_State_{i + 1}_s'] = FrameParser.gen_load_states(payload[i + 4] & 0x07)
+    #     data[f'Cooler_IsError_{i + 1}'] = (payload[i + 4] >> 6) & 0x01
+    #     data[f'Cooler_IsActivated_{i + 1}'] = (payload[i + 4] >> 7) & 0x01
+    #
+    # return data
+    pass
+
+def test_parse_tmc_status5():
+    # data = {'charg_req': payload[0] & 0x01,  # 1.1 Charging Required
+    #         'cool_req': (payload[0] >> 1) & 0x01,  # 1.2 Cooling Required
+    #         'boost_req': (payload[0] >> 2) & 0x01,  # 1.3 Boost Requested
+    #         'read_chrg_state': (payload[0] >> 3) & 0x01,  # 1.4 Read Charging State
+    #         'pwr_finished_flags': (payload[0] >> 4) & 0x0F,  # 1.5-1.8 PwrMng Finished Flags
+    #         'run_1_id': payload[5] & 0x07,  # 6.1-3 UnitId
+    #         'run_1_charging_assigned': (payload[5] >> 3) & 0x01,  # 6.4 charging_assigned
+    #         'run_1_cooling_assigned': (payload[5] >> 4) & 0x01,  # 6.5 cooling_assigned
+    #         'run_2_id': payload[6] & 0x07,  # 7.1-3 UnitId
+    #         'run_2_charging_assigned': (payload[6] >> 3) & 0x01,  # 7.4 charging_assigned
+    #         'run_2_cooling_assigned': (payload[6] >> 4) & 0x01,  # 7.5 cooling_assigned
+    #         'run_3_id': payload[7] & 0x07,  # 7.1-3 UnitId
+    #         'run_3_charging_assigned': (payload[7] >> 3) & 0x01,  # 7.4 charging_assigned
+    #         'run_3_cooling_assigned': (payload[7] >> 4) & 0x01}  # 6-8.5 cooling_assigned
+    # finished_flags = (payload[0] >> 4) & 0x0F
+    # for i in range(1, 9):
+    #     data[f'pwr_finished_flag_STATE_{i}'] = (finished_flags == i)
+    # return data
+    pass
+
+def test_parse_tmc2acu():
+    # data = {f'tmc_req_{channel}_s': FrameParser.gen_tmc_request((payload[0] >> 0) & 0x0F),  # 1.1-1.4 N/A TMC Request
+    #         f'eng_running_{channel}': (payload[1] >> 0) & 0x01,  # 2.1 N/A Engine running cmd
+    #         f'charging_running_{channel}': (payload[1] >> 1) & 0x01,  # 2.2 N/A Charging running cmd
+    #         f'cooling_running_{channel}': (payload[1] >> 3) & 0x01,  # 2.4 N/A Cooling running cmd
+    #         f'dpf_running_{channel}': (payload[1] >> 4) & 0x01,  # 2.5 N/A DPF running cmd
+    #         f'relief_valve_{channel}': (payload[1] >> 7) & 0x01,  # 2.8 N/A Relief Valve cmd
+    #         f'eng_speed_{channel}': payload[4] | (payload[5] << 8)}  # 5-6 N/A Engine Speed
+    #
+    # return data
+    pass
+
+def test_parse_tmc2emcu():
+    # data = {f'energy_source_select_{channel}': payload[1] & 0x03,  # 2.1-2.2 N/A Energy Source Select cmd
+    #         f'elmot_running_{channel}': (payload[1] >> 2) & 0x01,  # 2.3 N/A Elmot running cmd
+    #         f'elmot_charging_running_{channel}': (payload[1] >> 3) & 0x01,  # 2.4 N/A Elmot charging running cmd
+    #         f'aux_param_{channel}': payload[2]}  # 3 N/A AuxParam
+    #
+    # return data
+    pass
+
 def test_parse_acu_status():
+    # data = {f'diesel_state_{channel}': (payload[0] >> 4) & 0x0F,  # 1.5-1.8 Diesel State
+    #         f'diesel_state_{channel}_s': FrameParser.gen_diesel_state((payload[0] >> 4) & 0x0F),
+    #         f'dpf_state_{channel}_s': FrameParser.gen_dpf_state(payload[1] & 0x0F),
+    #         f'cooling_active_{channel}': (payload[1] >> 6) & 0x01,  # 2.7 Cooling Active
+    #         f'relief_valve_enable_{channel}': (payload[1] >> 7) & 0x01,  # 2.8 Relief Valve Enable
+    #         f'low_pressure_{channel}': (4.375 * (payload[5] / 10.0) - 18.3) / 10.0,
+    #         f'high_pressure_{channel}': (18.75 * (payload[6] / 10.0) - 75) / 10.0}
+    #
+    # return data
     payload = [20, 85, 0, 0, 0, 10, 20, 0] # Test data
     result = FrameParser.parse_acu_status(1, payload)
     assert result["diesel_state_1"] == 1
     assert result["cooling_active_1"] == 1
     assert "low_pressure_1" in result
     assert "high_pressure_1" in result
+
+def test_parse_acu_status2():
+    # data = {f'acu_supply_{channel}': payload[1] * 0.125}
+    #
+    # return data
+    pass
+
+def test_parse_acu_status3():
+    # data = {f'aftertreatment_diesel_particulate_filter_passive_regeneration_status_{channel}': payload[1] & 0x03,
+    #         f'aftertreatment_diesel_particulate_filter_active_regeneration_status_{channel}': (payload[1] >> 2) & 0x03,
+    #         f'dpf_active_regeneration_inhibited_status_{channel}': payload[2] & 0x03,
+    #         f'dpf_active_regeneration_inhibited_due_to_inhibit_switch_by_CAN_{channel}': (payload[2] >> 2) & 0x03,
+    #         f'dpf_active_regeneration_inhibited_due_to_neutral_{channel}': (payload[3] >> 4) & 0x03,
+    #         f'dpf_active_regeneration_inhibited_due_to_parking_brake_{channel}': payload[4] & 0x03,
+    #         f'dpf_active_regeneration_inhibited_system_fault_{channel}': (payload[4] >> 4) & 0x03,
+    #         f'dpf_active_regeneration_inhibited_system_timeout_{channel}': (payload[4] >> 6) & 0x03,
+    #         f'dpf_active_regeneration_inhibited_system_lockout_{channel}': (payload[5] >> 2) & 0x03,
+    #         f'dpf_active_regeneration_inhibited_engine_not_warmed_up_{channel}': (payload[5] >> 4) & 0x03,
+    #         f'exhaust_high_temperature_lamp_command_{channel}': (payload[6] >> 2) & 0x07,
+    #         f'dpf_active_regeneration_forced_status_{channel}': (payload[6] >> 5) & 0x07,
+    #         f'dpf_conditions_not_met_{channel}': (payload[7] >> 4) & 0x02}
+    #
+    # return data
+    pass
+
+def test_parse_acu_engine_status():
+    # data = {f'diesel_torque_{channel}': payload[0] - 125,
+    #         f'diesel_coolant_temp_{channel}': payload[2] - 40,
+    #         # 3 Engine Coolant Temperature (1 deg C/bit, -40 deg C offset)
+    #         f'diesel_moto_hours_{channel}': payload[4] | (payload[5] << 8) | (payload[6] << 16) | (payload[7] << 24)}  # 5-8 Engine Total Hours of Operation
+    #
+    # return data
+    pass
+
+def test_parse_acu_engine_status2():
+    # data = {f'dpf_time_{channel}': payload[0],
+    #         f'soot_{channel}': payload[1],
+    #         f'ash_{channel}': payload[2],
+    #         f'current_diesel_rpm_{channel}': payload[3] | (payload[4] << 8)}  # 4-5 1 rpm /bit, Offset 0
+    #
+    # return data
+    pass
+
+def test_parse_acu_error():
+    # data = {}
+    #
+    # data[f'Acu_Error_{channel}'] = payload[0] & 0x0F
+    # data[f'Cooling_Error_{channel}'] = (payload[0] >> 4) & 0x0F
+    # data[f'Charging_Error_{channel}'] = payload[1] & 0x0F
+    # data[f'Intake_Servo_Error_{channel}'] = (payload[1] >> 4) & 0x01
+    # data[f'Outtake_Servo_Error_{channel}'] = (payload[1] >> 5) & 0x01
+    # data[f'Diesel_Trouble_Codes_Total_Num_{channel}'] = payload[2]
+    #
+    # return data
+    pass
+
+def test_parse_parse_emcu_status():
+    # data = {f'EMCU_State_{channel}': payload[0] & 0x0F,
+    #         f'Running_{channel}': (payload[0] >> 4) & 0x01,
+    #         f'Charging0_{channel}': (payload[0] >> 5) & 0x01,
+    #         f'Source_Available_{channel}': payload[1] & 0x03,
+    #         f'Charging1_{channel}': (payload[0] >> 7) & 0x01,
+    #         f'Source_Selected_{channel}': (payload[1] >> 2) & 0x03,
+    #         f'Emergency_Run_{channel}': (payload[1] >> 4) & 0x01,
+    #         f'Emergency_Elmot_Block_{channel}': (payload[1] >> 5) & 0x01,
+    #         f'On_Run_{channel}': (payload[1] >> 6) & 0x01,
+    #         f'Bypassed_{channel}': (payload[1] >> 7) & 0x01,
+    #         f'Ok_Softstarter_{channel}': (payload[2] >> 0) & 0x01,
+    #         f'Overload_Softstarter_{channel}': (payload[2] >> 1) & 0x01,
+    #         f'Manual_On_{channel}': (payload[2] >> 2) & 0x01,
+    #         f'Manual_Off_{channel}': (payload[2] >> 3) & 0x01,
+    #         f'Elmot_State_{channel}': (payload[2] >> 4) & 0x0F,
+    #         f'Door_{channel}': (payload[3] >> 0) & 0x01}
+    #
+    # return data
+    pass
+
+def test_parse_parse_emcu_error():
+    # data = {f'Energy_Source_1_Error_{channel}': payload[0] & 0x0F,
+    #         f'Energy_Source_2_Error_{channel}': (payload[0] >> 4) & 0x0F,
+    #         f'Elmot_Charge_Error_{channel}': payload[1] & 0x0F,  # 2.1-2.4 charge error
+    #         f'Elmot_Error_{channel}': payload[2] & 0x0F}  # 3-4 Elmot error
+    #
+    # return data
+    pass
+
+def test_parse_acu_diagnostics2():
+    # data = {f'SRV_ISOLATE_IN_{channel}': (payload[0] >> 0) & 0x01,
+    #         f'SRV_ISOLATE_FB_{channel}': (payload[0] >> 1) & 0x01,
+    #         f'MCU_SRV_SUP_ENA_{channel}': (payload[0] >> 2) & 0x01,
+    #         f'SRV_SUP_ENA_{channel}': (payload[0] >> 3) & 0x01,
+    #         f'MCU_OE1_{channel}': (payload[0] >> 4) & 0x01,
+    #         f'SRV_A_OPEN_DRIVE_{channel}': (payload[0] >> 5) & 0x01,
+    #         f'SRV_B_OPEN_DRIVE_{channel}': (payload[0] >> 6) & 0x01,
+    #         f'SRV_A_DIV_ENA_{channel}': (payload[0] >> 7) & 0x01,
+    #         f'SRV_B_DIV_ENA_{channel}': (payload[1] >> 0) & 0x01,
+    #         f'SRV_SUP_ADC_{channel}': payload[2] | (payload[3] << 8)}
+    #
+    # return data
+    pass
 
 # Dummy frame
 class DummyFrame:
